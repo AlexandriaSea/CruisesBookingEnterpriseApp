dee198e524c553ba58d5e143e417c7c78a11a550
Wenjie Zhou <jack-zwj@hotmail.com>
2024-02-22 00:43:20 -0500

    Add registration page

--- src/main/java/com/wenjiezhou/assignment2/controllers/PassengerController.java
+++ src/main/java/com/wenjiezhou/assignment2/controllers/PassengerController.java
@@ -1,7 +1,22 @@
 package com.wenjiezhou.assignment2.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 
+import com.wenjiezhou.assignment2.models.Passenger;
+import com.wenjiezhou.assignment2.repositories.PassengerRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+@Controller
 public class PassengerController {
 
+    @Autowired
+    private PassengerRepository passRepository;
 }
